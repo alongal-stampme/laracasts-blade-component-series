080b3e11202fb70e5890770a51e3adff41de9f5d
Alon Gal <alon@stampme.com>
2020-06-12 21:03:00 +1000

    Modal Component

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,9 +1,22 @@
 <x-layout>
-    <x-flash type="error">
-        You are now logged out!
-    </x-flash>
-    <br/>
-    <x-flash class="mt-10" data-flash="foobar">
-        Great Job
-    </x-flash>
+    <x-modal
+        title="Deactivate Your Account?"
+        submit-label="Deactivate"
+    >
+        <x-slot name="trigger">
+            <button @click="on = true">Show Modal</button>
+        </x-slot>
+        Are you really sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+    </x-modal>
+
+    <br>
+
+    <x-modal
+        title="Subscribe"
+    >
+        <x-slot name="trigger">
+            <button @click="on = true">Click Me</button>
+        </x-slot>
+        lorem ipsm
+    </x-modal>
 </x-layout>
