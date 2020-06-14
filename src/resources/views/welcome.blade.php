d8fdf7568ad9e677942ca17a3cfedd29fccf6e7d
Alon Gal <alon@stampme.com>
2020-06-14 11:32:03 +1000

    Dropdown Component

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,22 +1,17 @@
 <x-layout>
-    <x-modal
-        title="Deactivate Your Account?"
-        submit-label="Deactivate"
-    >
-        <x-slot name="trigger">
-            <button @click="on = true">Show Modal</button>
-        </x-slot>
-        Are you really sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
-    </x-modal>
+    <x-section>
+        <div class="flex">
+            <x-dropdown>
+                <x-slot name="trigger">
+                    <button>Click Me</button>
+                </x-slot>
 
-    <br>
+                <x-dropdown-link href="/">One</x-dropdown-link>
+                <x-dropdown-link href="/">Two</x-dropdown-link>
+                <x-dropdown-link href="/">Three</x-dropdown-link>
 
-    <x-modal
-        title="Subscribe"
-    >
-        <x-slot name="trigger">
-            <button @click="on = true">Click Me</button>
-        </x-slot>
-        lorem ipsm
-    </x-modal>
+            </x-dropdown>
+        </div>
+
+    </x-section>
 </x-layout>
